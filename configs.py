355c917748732ad535408d72b9e0c1c5ed34d1fb
SMK-BOTZZ <161198246+SMK-BOTZZ@users.noreply.github.com>
2024-03-03 13:39:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,8 +9,8 @@ class Config:
     API_ID = int(getenv("API_ID", "28243586"))
     API_HASH = getenv("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
     BOT_TOKEN = getenv("BOT_TOKEN", "6812106698:AAGtDaint6SnowdD8tvgIEktg6Q-hzzhg50")
-    FSUB = getenv("FSUB", "SMK_NETWORK")
-    CHID = int(getenv("CHID", "-1002001054562"))
+    FSUB = getenv("FSUB", "")
+    CHID = int(getenv("CHID", ""))
     SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://madarazbotz:ZbVCR4tEzvjolZTj@cluster0.gfqwloi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
